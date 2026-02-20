a391275dd18be34790818d1c7d179023b82b8296
Shraddha Sasikumar <shraddhaskumar04@gmail.com>
2026-02-20 14:59:52 +0530

    Test CI Docker build & push

--- .history/app_20260220135150.py
+++ .history/app_20260220135150.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 1: Hello from CI + Trivy + K8s!"
+    return "Hello from CI + Trivy + K8s!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
